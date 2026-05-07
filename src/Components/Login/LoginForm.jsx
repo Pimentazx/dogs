8990064ebbf690a8a053dbda0ad7da4111835969
Guilherme Pimentel <gui.pimentel2004@gmail.com>
2026-05-07 17:19:14 -0300

    fix: Compenents Form and Hook Form

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,19 +1,25 @@
 import { func } from "prop-types";
 import React from "react";
 import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import UserForm from "../../../Hooks/userForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
+  const username = UserForm();
+  const password = UserForm();
+  
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+
     fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify(),
     })
       .then((response) => {
         console.log(response);
@@ -24,24 +30,15 @@ const LoginForm = () => {
         return json;
       });
   }
+}
 
   return (
   <section>
     <h1>Login</h1>
     <form action="" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={username}
-        onChange={({ target }) => setUsername(target.value)}
-        placeholder="Usuário"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={({ target }) => setPassword(target.value)}
-        placeholder="Senha"
-      />
-      <button>Enviar</button>
+      <Input label="Usuário" type="text" name="username" {...username}/>
+      <Input label="Senha" type="password" name="password" {...password} />
+      <Button>Entrar</Button>
     </form>
     <Link to="/login/criar">Cadrastro</Link>
   </section>
