f99a584575c26d9d39d746352212b76193c3c4ec
Guilherme Pimentel <gui.pimentel2004@gmail.com>
2026-05-18 19:27:55 -0300

    create: protected route

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserStorage } from "./UserContext.jsx";
+import ProtectedRoute from "./Components/Helpers/ProtectedRoute.jsx";
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <Route path="/conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
           </Routes>
           <Footer />
           {/* <Api/>  */}
---
+++ src/Components/Helpers/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { UserContext } from "../../UserContext.jsx";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) {
+    return children;
+  } else if (login === false) {
+    return <Navigate to="/login" />;
+  } else {
+    return null;
+  }
+
+  return <></>;
+};
+
+export default ProtectedRoute;
+
