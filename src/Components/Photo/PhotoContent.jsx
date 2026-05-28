c484c2e25c6e3a0656053193ccab231c50e33d2a
Guilherme Pimentel <gui.pimentel2004@gmail.com>
2026-05-28 17:50:50 -0300

    create: PhotoDelete and SkeletonImage

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
+import { UserContext } from "../../UserContext";
+import Image from "../Helpers/Image";
+import PhotoDelete from "./PhotoDelete";
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image img src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} /> 
+            ) : (
+            < Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          )}
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacao}>{photo.acessos}</span>
           </p>
