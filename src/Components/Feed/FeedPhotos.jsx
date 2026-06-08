28816084a12e4453cd241defb7c271a49f0ce0be
Guilherme Pimentel <gui.pimentel2004@gmail.com>
2026-06-08 15:41:23 -0300

    feat: add scroll infinite

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,27 +6,34 @@ import Error from "../Helpers/Error";
 import Loading from "../Helpers/Loading";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({ user, page, setModalPhoto, setInfinite }) => {
+  const { data, loading, error, request } = useFetch();
 
-    const {data, loading, error, request} = useFetch();
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) setInfinite(false);
+    }
+    fetchPhotos();
+  }, [request, user, page, setInfinite]);
 
-    React.useEffect(() => {
-        async function fetchPhotos() {
-            const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0});
-            const {response, json} =  await request(url, options);
-        }
-        fetchPhotos();
-    }, [request]);
-
-    if(error) return <Error error={error} />;
-    if(loading) return <Loading/>;
-    if(data)
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
     return (
-        <ul className={`${styles.feed} animeLeft`}>
-            {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>)}
-        </ul>
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
+      </ul>
     );
-    else return null;
-}   
+  else return null;
+};
 
 export default FeedPhotos;
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -6,11 +6,14 @@ import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
 
 const User = () => {
+    const {data} = React.useContext(UserContext);
+
+
     return (
         <section className="container">
             <UserHeader />
             <Routes>
-                <Route path="/" element={<Feed/>} />
+                <Route path="/" element={<Feed user={data.id}/>} />
                 <Route path="postar" element={<UserPhotoPost/>} />
                 <Route path="estatisticas" element={<UserStats/>} />
             </Routes>
