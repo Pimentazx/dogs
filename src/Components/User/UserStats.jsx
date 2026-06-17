f1e3d954c2628267c6519bfe44ceb9ba2e1d01f7
Guilherme Pimentel <gui.pimentel2004@gmail.com>
2026-06-17 13:34:43 -0300

    feat: react components, finals adjusts and lazy e suspense

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../Hooks/useFetch";
 import { STATS_GET } from "../../api";
 import Loading from "../Helpers/Loading";
 import Error from "../Helpers/Error";
-import UserStatsGraphs from "./UserStatsGraphs";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 
 const UserStats = () => {
@@ -22,10 +22,10 @@ const UserStats = () => {
     if (error) return <Error error={error}/>;
     if(data)
     return (
-        <div>
+        <React.Suspense fallback={<div></div>}>
             <Head title="Estatísticas"/>
             <UserStatsGraph data={data} />
-        </div>
+        </React.Suspense>
     );
     else return null;
 }
